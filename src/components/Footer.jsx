b0f784262657a12bf51c3179f3a92473a9ea4efe
Lincoln Gibson <lincolngibson7@gmail.com>
2023-11-20 20:50:41 -0300

    Add footer component.

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer className="flex justify-center items-center h-16 bg-black text-white relative bottom-0">
+    <p className="text-center text-sm sm:text-base">
+      &copy; 2023 by Lincoln W Gibson All Rights Reserved.
+    </p>
+  </footer>
+);
+
+export default Footer;

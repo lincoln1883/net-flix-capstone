fe1a2bff3b0553053f1daa940c8e1e39e4373e13
Lincoln Gibson <lincolngibson7@gmail.com>
2023-11-20 20:54:55 -0300

    Add footer import.

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from './Navbar';
+import Footer from './Footer';
 
 const Layout = () => (
   <>
     <Navbar />
     <Outlet />
+    <Footer />
   </>
 
 );
